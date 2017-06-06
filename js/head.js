c2491eb060aca919cd9bc442d2b2d686ac88b499
feilengcui008 <feilengcui008@gmail.com>
2017-06-06 15:06:04 +0800

    update

--- js/head.js
+++ js/head.js
@@ -1,8 +1,8 @@
 function mouseOver() {
   document.getElementsByClassName('sidebar-toggle-1')[0].style.display = "block";
-  //document.getElementsByClassName('sidebar-toggle-2')[0].style.display = "block";
+  document.getElementsByClassName('sidebar-toggle-2')[0].style.display = "block";
 }
 function mouseOut() {
   document.getElementsByClassName('sidebar-toggle-1')[0].style.display = "none";
-  //document.getElementsByClassName('sidebar-toggle-2')[0].style.display = "none";
+  document.getElementsByClassName('sidebar-toggle-2')[0].style.display = "none";
 }
